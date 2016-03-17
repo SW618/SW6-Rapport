b0dec84ab20da2ab11af8e34e8a3d2d772f26537
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-03-17 13:19:56 +0100

    dont detect bib

--- findUnused.sh
+++ findUnused.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sh getOrder.sh $1 > .usedfiles
-git ls-files | egrep -v '^\.|\.sh$|\.md$|Makefile' > .trackedreportfiles
+git ls-files | egrep -v '^\.|\.sh$|\.bib$|\.md$|Makefile' > .trackedreportfiles
 echo 'Unused files:'
 grep -Fxv -f .usedfiles .trackedreportfiles
 rm -f .usedfiles .trackedreportfiles
