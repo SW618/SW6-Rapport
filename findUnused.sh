968390e7befd2a786732b4e5ec3bed90d1cd36ab
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-03-17 17:29:39 +0100

    advanced scriptzz

--- findUnused.sh
+++ findUnused.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-sh getOrder.sh $1 > .usedfiles
-git ls-files | egrep -v '^\.|\.sh$|\.bib$|\.md$|Makefile' > .trackedreportfiles
-echo 'Unused files:'
-grep -Fxv -f .usedfiles .trackedreportfiles
-rm -f .usedfiles .trackedreportfiles
+TEXROOT=${1:-master.tex}
+sh getOrder.sh $TEXROOT > .usedfiles
+if [ $? -eq 0 ];
+then
+    git ls-files | egrep -v '^\.|\.sh$|\.bib$|\.md$|Makefile' > .trackedreportfiles
+    echo 'Unused files:'
+    grep -Fxv -f .usedfiles .trackedreportfiles
+    rm -f .usedfiles .trackedreportfiles
+    exit 0
+else
+    cat .usedfiles
+    rm -f .usedfiles
+    exit 1
+fi
